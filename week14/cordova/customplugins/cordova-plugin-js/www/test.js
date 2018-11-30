b4db91e9a33d5958b5e7c8ffd6c48d57deaea159
ancientlives <ancientlives@gmail.com>
2018-11-29 18:24:03 -0600

    update source for week 14

---
+++ week14/cordova/customplugins/cordova-plugin-js/www/test.js
@@ -0,0 +1,12 @@
+module.exports = {
+
+  // get daily note
+  dailyNote: function() {
+      return "a daily travel note to inspire a holiday...";
+  },
+
+  // get daily note via the callback function
+  dailyNoteCall: function (noteCall) {
+    noteCall("a daily travel note to inspire a holiday...");
+  }
+};
